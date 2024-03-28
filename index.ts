d2cebcc805ecdb60e93adf1356137e7c477a2cae
shivam_agrawal_tal <v-shivam.agrawal@lightfinance.com>
2024-03-28 14:21:36 +0530

    refactor npm package

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { ApiLoggerMiddleware, getMobileNumberFromJwtToken } from "./src/logger/logger.service";
+import { Logger } from "winston";
+
+export {
+    Logger,
+    ApiLoggerMiddleware,
+    getMobileNumberFromJwtToken,
+};
